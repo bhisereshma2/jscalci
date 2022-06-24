97454c14ddcd88b6a0d5e42e1c60b795690c0e22
bhisereshma2 <bhisereshma2@gmail.com>
2022-06-24 18:48:40 +0530

    new commit

---
+++ calci.js
@@ -0,0 +1,19 @@
+const num1 = parseFloat(prompt('Enter number 1: '));
+const num2 = parseFloat(prompt('Enter number 2: '));
+const operator = prompt('Enter operator (+, -, /, *)');
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert('Wrong Input! Refresh the page again and provide data');
+}else{
+
+    if(operator == '+'){
+        result= num1 + num2;
+    }else if(operator == '-'){
+        result = num1 - num2;
+    }else if(operator == '*'){
+        result = num1 * num2;
+    }else if(operator == '/'){
+        result = num1 / num2;
+    }
+    document.write(num1 + operator + num2 +' = ' + result);
+}
